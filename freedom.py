b39af52642b682cb658681665ef0d5ae1556dbe7
jsmit260 <josh.smith.it@gmail.com>
2020-05-28 13:07:06 -0400

    scrubbing and validation

--- freedom.py
+++ freedom.py
@@ -1,10 +1,24 @@
 #! /usr/bin/python3
 import os
-import sys
+import re
+
+def isValid(x):
+    ok = re.search("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",x)
+    if ok:
+        return True
+    else:
+        return False
+
 i = input("Enter IP Addresses in the path to take each separated by a comma: ")
 path  = i.split(",")
+j = [i.strip() for i in path]
+v = [isValid(i) for i in j]
+if False in v:
+    print("At least one invalid IP address given. Try again.")
+    exit()
 bs = 'ssh -R 9050 -J'
 et = ''
-c = [ et+' root@{}'.format(i) for i in path]
+c = [ et+' root@{}'.format(i) for i in j]
 f=bs+' '.join(c)
+print(f)
 os.system(f)
